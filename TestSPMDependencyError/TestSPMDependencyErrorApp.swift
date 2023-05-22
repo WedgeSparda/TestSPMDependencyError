d18b97f129934e0f6ae371ee5728a393c1a5ae65
Roberto Pastor <roberto.pastor@cabify.com>
2023-05-22 17:07:43 +0200

    Initial Commit

---
+++ TestSPMDependencyError/TestSPMDependencyErrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSPMDependencyErrorApp.swift
+//  TestSPMDependencyError
+//
+//  Created by Roberto Pastor on 22/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TestSPMDependencyErrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
